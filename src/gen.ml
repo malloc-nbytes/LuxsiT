ad0402e84760263c55519d62b385392c04ea39c4
malloc-nbytes <zdhdev@yahoo.com>
2023-08-25 22:34:06 -0500

    variables now work

--- src/gen.ml
+++ src/gen.ml
@@ -1,21 +1,57 @@
+type var_t =
+  { stackloc : int }
+
 type gen_t =
   { output : string;
-    stackptr : int }
+    stackptr : int;
+    vars : (string, var_t) Hashtbl.t }
 
 let err (msg : string) : unit =
   Printf.printf "(ERR) %s\n" msg
+
+let var_exists (gen : gen_t) (name : string) : bool =
+  Hashtbl.mem gen.vars name
+
+let get_var (gen : gen_t) (name : string) : var_t option =
+  if var_exists gen name then
+    let var = Hashtbl.find gen.vars name in
+    Some var
+  else
+    None
+
+let insert_var (gen : gen_t) (name : string) : unit =
+  let v = { stackloc = gen.stackptr } in
+  Hashtbl.add gen.vars name v
+
+let push (gen : gen_t) (reg : string) : gen_t =
+  let output = gen.output in
+  let output = output ^ "    push " ^ reg ^ "\n" in
+  { gen with output = output; stackptr = gen.stackptr + 1 }
+
+let pop (gen : gen_t) (reg : string) : gen_t =
+  let output = gen.output in
+  let output = output ^ "    pop " ^ reg ^ "\n" in
+  { gen with output = output; stackptr = gen.stackptr - 1 }
 
 let gen_expr (gen : gen_t) (expr : Parser.node_expr_t) : gen_t =
   match expr with
   | Parser.NodeExprIntlit expr_intlit -> 
     let output = gen.output in
     let output = output ^ "    mov rax, " ^ expr_intlit.intlit.data ^ "\n" in
-    let output = output ^ "    push rax\n" in
-    { gen with output = output }
-  | Parser.NodeExprId id -> 
-    let output = gen.output in
-    (* todo *)
-    { gen with output = output }
+    push ({ gen with output = output }) "rax"
+  | Parser.NodeExprId expr_id ->
+
+    let var : var_t =
+      match get_var gen expr_id.id.data with
+      | Some var -> var
+      | None ->
+         let _ = err ("undeclared ID " ^ expr_id.id.data ^ "\n") in
+         failwith "gen error" in
+
+    let offset = string_of_int ((gen.stackptr - var.stackloc - 1) * 8) in
+    let output = gen.output ^ "    mov rax, QWORD [rsp + " ^ offset ^ "]\n" in
+    let gen = push { gen with output = output } "rax" in
+    gen
 
 let generate_stmt (gen : gen_t) (stmt : Parser.node_stmt_t) : gen_t =
   match stmt with
@@ -23,10 +59,15 @@ let generate_stmt (gen : gen_t) (stmt : Parser.node_stmt_t) : gen_t =
     let gen = gen_expr gen stmt_exit in
     let output = gen.output in
     let output = output ^ "    mov rax, 60\n" in
-    let output = output ^ "    pop rdi\n" in
-    let output = output ^ "    syscall\n" in
-    { gen with output = output }
-  | NodeStmtLet stmt_let -> failwith "unimplemented"
+    let gen = pop ({ gen with output = output }) "rdi" in
+    { gen with output = gen.output ^ "    syscall\n" }
+  | NodeStmtLet stmt_let ->
+     if var_exists gen stmt_let.id.data then
+       let _ = err ("ID " ^ stmt_let.id.data ^ " is already defined") in
+       failwith "gen error"
+     else
+       let _ = insert_var gen stmt_let.id.data in
+       gen_expr gen stmt_let.expr
 
 let generate_program (program : Parser.node_prog_t) : string =
   let rec iter_prog_stmts (gen : gen_t) (lst : Parser.node_stmt_t list) : gen_t =
@@ -35,7 +76,9 @@ let generate_program (program : Parser.node_prog_t) : string =
     | hd :: tl -> iter_prog_stmts (generate_stmt gen hd) tl
   in
 
-  let gen = { output = "global _start\n_start:\n"; stackptr = 0 } in
+  let gen = { output = "global _start\n_start:\n";
+              stackptr = 0;
+              vars = Hashtbl.create 20 } in
 
   let gen = iter_prog_stmts gen program.stmts in
 
@@ -43,5 +86,5 @@ let generate_program (program : Parser.node_prog_t) : string =
   let output = output ^ "    mov rax, 60\n" in
   let output = output ^ "    mov rdi, 0\n" in
   let output = output ^ "    syscall" in
-  
+
   output
--- src/parser.ml
+++ src/parser.ml
@@ -35,13 +35,19 @@ let parser_create (tokens : Token.token_t list) : parser_t =
 let peek (p : parser_t) : Token.token_t option =
   match p.tokens with
   | [] -> None
-  | hd :: _ -> Some hd
+  | hd :: _ ->
+     let type_ = Token.get_tokentype_as_str hd.tokentype in
+     let _ = Printf.printf "peeking: %s\n" type_ in
+     Some hd
 
 (* Will return the token at `hd`. Will eat it. *)
 let eat (p : parser_t) : parser_t * (Token.token_t option) =
   match p.tokens with
   | [] -> p, None
-  | hd :: tl -> parser_create tl, Some hd
+  | hd :: tl ->
+     let type_ = Token.get_tokentype_as_str hd.tokentype in
+     let _ = Printf.printf "eating: %s\n" type_ in
+     parser_create tl, Some hd
 
 (* Will eat the token at `hd`. If `hd.tokentype` does not match
    what is expected, will throw an error. *)
@@ -51,6 +57,8 @@ let expect (p : parser_t) (expected_type : Token.tokentype_t) : parser_t * Token
     let _ = err ("unexpected token \"" ^ hd.data ^ "\" expected: " ^ (Token.get_tokentype_as_str expected_type)) in
     failwith "parser error"
   else
+    let type_ = Token.get_tokentype_as_str hd.tokentype in
+    let _ = Printf.printf "expecting: %s\n" type_ in
     parser_create tl, hd
 
 let parse_expr (p : parser_t) : parser_t * (node_expr_t option) =
@@ -64,23 +72,28 @@ let parse_expr (p : parser_t) : parser_t * (node_expr_t option) =
      failwith "parser error"
 
 let parse_stmt (p : parser_t) : parser_t * (node_stmt_t option) =
+  print_endline "parse_stmt";
   match peek p with
   | Some t when t.tokentype = Token.Exit ->
+     print_endline "token.exit";
      let p, _ = eat p in        (* eat exit *)
      let p, _ = expect p Token.LParen in
      let p, node_expr = parse_expr p in
      (match node_expr with
       | Some expr ->
          let p, _ = expect p Token.RParen in
+         let p, _ = expect p Token.SemiColon in
          p, Some (NodeStmtExit expr)
       | None ->
          let _ = err "expected an expression after 'exit('" in
          failwith "parser error")
   | Some t when t.tokentype = Token.Let ->
+     print_endline "token.Let";
      let p, _ = eat p in        (* eat let *)
      let p, id = expect p Token.ID in
      let p, _ = expect p Token.Equals in
      let p, expr = parse_expr p in
+     let p, _ = expect p Token.SemiColon in
      (match expr with
       | Some e -> p, Some (NodeStmtLet { id = id; expr = e })
       | None ->
@@ -89,10 +102,11 @@ let parse_stmt (p : parser_t) : parser_t * (node_stmt_t option) =
   | _ -> p, None (* no stmt found *)
 
 let parse_program (p : parser_t) : node_prog_t =
-  let rec parse_program' (p : parser_t) (program : node_prog_t) : node_prog_t =
-    let p, stmt = parse_stmt p in
+  let rec parse_program' (p : parser_t) (program : node_prog_t) : parser_t * node_prog_t =
+    let p', stmt = parse_stmt p in
     match stmt with
-    | Some stmt -> parse_program' p { stmts = program.stmts @ [stmt] }
-    | None -> program
+    | Some stmt -> parse_program' p' { stmts = program.stmts @ [stmt] }
+    | None -> p', program
   in
-  parse_program' p { stmts = [] }
+  let final_parser_state, result = parse_program' p { stmts = [] } in
+  result
