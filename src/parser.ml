8984e65c76a7a59c8ef9750165be4a4b8ff9afed
malloc-nbytes <zdhdev@yahoo.com>
2023-09-03 16:15:20 -0500

    added debugging print functions

--- src/parser.ml
+++ src/parser.ml
@@ -19,12 +19,13 @@ and node_bin_expr_t =
 and node_term_t =
   | NodeTermIntlit of node_term_intlit_t
   | NodeTermId of node_term_id_t
+  | NodeTermParen of unit
 
 and node_expr_t =
   | NodeTerm of node_term_t
   | NodeBinaryExpr of node_bin_expr_t
 
-type node_stmt_let_t =
+and node_stmt_let_t =
   { id : Token.token_t;
     expr : node_expr_t }
 
@@ -52,6 +53,30 @@ let unwrap (k : 'a option) : 'a =
      let _ = err "called unwrap on None value" in
      failwith "(fatal) parser error"
 
+let print_tokens (tokens : Token.token_t list) : unit =
+  let _ = List.iter (fun t -> Printf.printf "%s" ((Token.get_tokentype_as_str t.Token.tokentype))) tokens in
+  print_endline ""
+
+let rec print_expr (expr : node_expr_t) : unit =
+  let print_term (term : node_term_t) : unit =
+    match term with
+    | NodeTermIntlit intlit -> print_endline ("NodeTermIntlit: " ^ intlit.intlit.data)
+    | NodeTermId id -> print_endline ("NodeTermId: " ^ id.id.data)
+    | NodeTermParen () -> print_endline "NodeTermParen" in
+  let print_bin_expr (bin_expr : node_bin_expr_t) : unit =
+    match bin_expr with
+    | NodeBinExprAdd add -> 
+        print_endline "NodeBinExprAdd";
+        print_expr add.lhs;
+        print_expr add.rhs
+    | NodeBinExprMult mult -> 
+        print_endline "NodeBinExprMult";
+        print_expr mult.lhs;
+        print_expr mult.rhs in
+  match expr with
+  | NodeTerm term -> print_term term
+  | NodeBinaryExpr bin_expr -> print_bin_expr bin_expr
+
 (* Will return the token at `hd`. Does not eat it.*)
 let peek (p : parser_t) : Token.token_t option =
   match p.tokens with
@@ -60,6 +85,8 @@ let peek (p : parser_t) : Token.token_t option =
 
 (* Will return the token at `hd`. Will eat it. *)
 let eat (p : parser_t) : parser_t * (Token.token_t option) =
+  let _ = print_endline ("eat: " ^ (Token.get_tokentype_as_str (unwrap (peek p)).tokentype)) in
+  let _ = print_tokens p.tokens in
   match p.tokens with
   | [] -> p, None
   | hd :: tl -> parser_create tl, Some hd
@@ -67,6 +94,8 @@ let eat (p : parser_t) : parser_t * (Token.token_t option) =
 (* Will eat the token at `hd`. If `hd.tokentype` does not match
    what is expected, will throw an error. *)
 let expect (p : parser_t) (expected_type : Token.tokentype_t) : parser_t * Token.token_t =
+  let _ = print_endline ("expect: " ^ (Token.get_tokentype_as_str expected_type)) in
+  let _ = print_tokens p.tokens in
   let hd = List.hd p.tokens and tl = List.tl p.tokens in
   if hd.tokentype <> expected_type then
     let _ = err ("unexpected token \"" ^ hd.data ^ "\" expected: " ^ (Token.get_tokentype_as_str expected_type)) in
@@ -74,15 +103,21 @@ let expect (p : parser_t) (expected_type : Token.tokentype_t) : parser_t * Token
   else parser_create tl, hd
 
 let rec parse_term (p : parser_t) : parser_t * (node_term_t option) =
+  let _ = print_endline "parse_term" in
+  let _ = print_tokens p.tokens in
   match peek p with
+  (* Handle integer literals and identifiers *)
   | Some t when t.tokentype = Token.IntegerLiteral -> p, Some (NodeTermIntlit { intlit = t })
   | Some t when t.tokentype = Token.ID -> p, Some (NodeTermId { id = t })
+  | Some t when t.tokentype = Token.LParen -> p, Some (NodeTermParen ())
   | None -> p, None
   | _ ->
     let _ = err ("unexpected token \"" ^ (Token.get_tokentype_as_str (unwrap (peek p)).tokentype) ^ "\" in term") in
     failwith "parser error"
 
 and parse_multiplicative_expr (p : parser_t) : parser_t * (node_expr_t option) =
+  let _ = print_endline "parse_multiplicative_expr" in
+  let _ = print_tokens p.tokens in
   let p, term = parse_term p in
   if term <> None then
     let p, _ = eat p in (* eat term i.e. 1 * 2, `1` is a term, it gets eaten. *)
@@ -100,6 +135,8 @@ and parse_multiplicative_expr (p : parser_t) : parser_t * (node_expr_t option) =
     p, None
 
 and parse_additive_expr (p : parser_t) : parser_t * (node_expr_t option) =
+  let _ = print_endline "parse_additive_expr" in
+  let _ = print_tokens p.tokens in
   let p, term = parse_multiplicative_expr p in
   if term <> None then
     (* let p, _ = eat p in *) (* do not eat here, does so in parse_multiplicative_expr *)
@@ -117,39 +154,50 @@ and parse_additive_expr (p : parser_t) : parser_t * (node_expr_t option) =
     p, None
 
 and parse_expr (p : parser_t) : parser_t * (node_expr_t option) =
+  let _ = print_endline "parse_expr" in
+  let _ = print_tokens p.tokens in
   let p, term = parse_term p in
-  if term <> None then
-    let p, _ = eat p in (* eat term i.e. 1 + 2, `1` is a term, it gets eaten. *)
-    match peek p with (* figure out which binary expression to use *)
-    | Some t when t.tokentype = Token.Plus ->
-       let p, _ = expect p Token.Plus in
-       let p, rhs = parse_additive_expr p in
-       (match rhs with
-        | Some rhs_expr -> p, Some (NodeBinaryExpr (NodeBinExprAdd { lhs = NodeTerm (unwrap term); rhs = rhs_expr }))
-        | None ->
-           let _ = err "expected expression after '+'" in
-           failwith "parser error")
-    | Some t when t.tokentype = Token.Mult ->
-       let p, _ = expect p Token.Mult in
-       let p, rhs = parse_multiplicative_expr p in
-       (match rhs with
-        | Some rhs_expr -> p, Some (NodeBinaryExpr (NodeBinExprMult { lhs = NodeTerm (unwrap term); rhs = rhs_expr }))
-        | None ->
-           let _ = err "expected expression after '*'" in
-           failwith "parser error")
-    | _ -> p, Some (NodeTerm (unwrap term))
-  else
-    p, None
+  match term with
+  | Some NodeTermParen _ ->
+      let p, _ = expect p Token.LParen in
+      let p, expr = parse_expr p in
+      let p, _ = expect p Token.RParen in
+      (match expr with
+      | Some expr -> p, Some expr
+      | None ->
+        let _ = err "expected expression after '(' in parenthetical expression" in
+        failwith "parser error")
+  | Some term ->
+      let p, _ = eat p in (* eat term i.e. 1 + 2, `1` is a term, it gets eaten. *)
+      (match peek p with (* figure out which binary expression to use *)
+      | Some t when t.tokentype = Token.Plus ->
+        let p, _ = expect p Token.Plus in
+        let p, rhs = parse_additive_expr p in
+        (match rhs with
+          | Some rhs_expr -> p, Some (NodeBinaryExpr (NodeBinExprAdd { lhs = NodeTerm term; rhs = rhs_expr }))
+          | None ->
+            let _ = err "expected expression after '+'" in
+            failwith "parser error")
+      | Some t when t.tokentype = Token.Mult ->
+        let p, _ = expect p Token.Mult in
+        let p, rhs = parse_multiplicative_expr p in
+        (match rhs with
+          | Some rhs_expr -> p, Some (NodeBinaryExpr (NodeBinExprMult { lhs = NodeTerm term; rhs = rhs_expr }))
+          | None ->
+            let _ = err "expected expression after '*'" in
+            failwith "parser error")
+      | _ -> p, Some (NodeTerm term))
+  | _ -> p, None
 
 and parse_stmt (p : parser_t) : parser_t * (node_stmt_t option) =
+  let _ = print_endline "parse_stmt" in
+  let _ = print_tokens p.tokens in
   match peek p with
   | Some t when t.tokentype = Token.Exit ->
      let p, _ = eat p in        (* eat exit *)
-     (* let p, _ = expect p Token.LParen in *)
      let p, node_expr = parse_expr p in
      (match node_expr with
       | Some expr ->
-         (* let p, _ = expect p Token.RParen in *)
          let p, _ = expect p Token.SemiColon in
          p, Some (NodeStmtExit expr)
       | None ->
@@ -157,11 +205,9 @@ and parse_stmt (p : parser_t) : parser_t * (node_stmt_t option) =
          failwith "parser error")
   | Some t when t.tokentype = Token.Println ->
      let p, _ = eat p in        (* eat println *)
-     (* let p, _ = expect p Token.LParen in *)
      let p, node_expr = parse_expr p in
      (match node_expr with
       | Some expr ->
-         (* let p, _ = expect p Token.RParen in *)
          let p, _ = expect p Token.SemiColon in
          p, Some (NodeStmtPrintln expr)
       | None ->
@@ -184,6 +230,8 @@ and parse_stmt (p : parser_t) : parser_t * (node_stmt_t option) =
   | _ -> p, None (* no stmt found *)
 
 let parse_program (p : parser_t) : node_prog_t =
+  let _ = print_endline "parse_program" in
+  let _ = print_tokens p.tokens in
   let rec parse_program' (p : parser_t) (program : node_prog_t) : parser_t * node_prog_t =
     let p', stmt = parse_stmt p in
     match stmt with
@@ -192,4 +240,3 @@ let parse_program (p : parser_t) : node_prog_t =
   in
   let _, result = parse_program' p { stmts = [] } in
   result
-
