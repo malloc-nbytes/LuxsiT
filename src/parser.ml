7dd549e579e79310786fe1c3e3a59d8c4e658f90
malloc-nbytes <zdhdev@yahoo.com>
2023-09-03 19:20:43 -0500

    op precidence and parens

--- src/parser.ml
+++ src/parser.ml
@@ -26,17 +26,29 @@ type parser_t =
 let err (msg : string) : unit =
   print_endline msg
 
+
+let expect (p : parser_t) (expected_type : Token.tokentype_t) : parser_t * Token.token_t =
+  match p.tokens with
+  | [] -> failwith "no more tokens"
+  | hd :: tl when hd.tokentype = expected_type -> { tokens = tl }, hd
+  | hd :: _ ->
+    let _ = err ("expected token " ^ (Token.get_tokentype_as_str expected_type) ^ " but got " ^ hd.data) in 
+    failwith "expected token"
+
+
 let eat (p : parser_t) : parser_t * Token.token_t =
   match p.tokens with
   | [] -> failwith "no more tokens"
   | hd :: tl -> { tokens = tl }, hd
+
 
 let at (p : parser_t) : Token.token_t =
   match p.tokens with
   | [] -> failwith "no more tokens"
   | hd :: _ -> hd
 
-let parse_primary_expr (p : parser_t) : parser_t * expr_t =
+
+let rec parse_primary_expr (p : parser_t) : parser_t * expr_t =
   match at p with
   | t when t.tokentype = Token.ID ->
     let p, t = eat p in
@@ -44,27 +56,47 @@ let parse_primary_expr (p : parser_t) : parser_t * expr_t =
   | t when t.tokentype = Token.IntegerLiteral ->
     let p, t = eat p in
     p, IntegerLiteral { value = t.data }
+  | t when t.tokentype = Token.LParen ->
+    let p, _ = expect p Token.LParen in
+    let p, expr = parse_expr p in
+    let p, _ = expect p Token.RParen in
+    p, expr
   | _ -> let _ = err ("unexpected token " ^ (at p).data) in failwith "unexpected token"
 
-(* TODO: This is a QAD iterative approach. Fix later to get purely functional. *)
-let parse_additive_expr (p : parser_t) : parser_t * expr_t =
+
+and parse_multiplicitave_expr (p : parser_t) : parser_t * expr_t =
   let p, lhs = parse_primary_expr p in
+  let rec parse_multiplicitave_expr (p : parser_t) (lhs : expr_t) : parser_t * expr_t =
+    match at p with
+    | t when t.tokentype = Token.Mult ->
+      let p, t = eat p in
+      let p, rhs = parse_primary_expr p in
+      parse_multiplicitave_expr p (BinaryExpr { left = lhs; right = rhs; operator = t.data })
+    | _ -> p, lhs in
+  parse_multiplicitave_expr p lhs
+
+
+and parse_additive_expr (p : parser_t) : parser_t * expr_t =
+  let p, lhs = parse_multiplicitave_expr p in
   let rec parse_additive_expr (p : parser_t) (lhs : expr_t) : parser_t * expr_t =
     match at p with
     | t when t.tokentype = Token.Plus ->
       let p, t = eat p in
-      let p, rhs = parse_primary_expr p in
+      let p, rhs = parse_multiplicitave_expr p in
       parse_additive_expr p (BinaryExpr { left = lhs; right = rhs; operator = t.data })
     | _ -> p, lhs in
   parse_additive_expr p lhs
 
-let parse_expr (p : parser_t) : parser_t * expr_t =
+
+and parse_expr (p : parser_t) : parser_t * expr_t =
   let p, expr = parse_additive_expr p in
   p, expr
+
 
 let parse_stmt (p : parser_t) : parser_t * stmt_t =
   let p, expr = parse_expr p in
   p, ExprStmt expr
+
 
 (* Entrypoint. *)
 let produce_ast (tokens : Token.token_t list) : program_t =
@@ -78,6 +110,7 @@ let produce_ast (tokens : Token.token_t list) : program_t =
       produce_program p { body = program.body @ [stmt] }
   in
   produce_program p { body = [] }
+
 
 (* Debugging. *)
 let rec print_expr (expr : expr_t) (indent : int) : unit =
@@ -95,6 +128,7 @@ let rec print_expr (expr : expr_t) (indent : int) : unit =
     Printf.printf "%s  Right:\n" indentation;
     print_expr bin_expr.right (indent + 1)
 
+
 let rec print_stmt (stmt : stmt_t) (indent : int) : unit =
   let indentation = String.make (2 * indent) ' ' in
   match stmt with
@@ -102,16 +136,18 @@ let rec print_stmt (stmt : stmt_t) (indent : int) : unit =
     Printf.printf "%sExprStmt:\n" indentation;
     print_expr expr (indent + 1)
 
+
 let rec print_program (program : program_t) (indent : int) : unit =
   let indentation = String.make (2 * indent) ' ' in
   Printf.printf "%sProgram:\n" indentation;
   List.iter (fun stmt -> print_stmt stmt (indent + 1)) program.body
+
 
 let print_program_structure (program : program_t) : unit =
   print_program program 0
 
 
 let () =
-  let lexer = Lexer.parse_code "10 + 5 + x" in
+  let lexer = Lexer.parse_code "10 + (5 * 3)" in
   let program = produce_ast lexer.tokens in
   print_program_structure program
